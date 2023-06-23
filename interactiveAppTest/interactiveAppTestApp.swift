86fdfb9637b90ae078d4f41f8013d3e6f0110991
Marianne Mai <mariannemai02@gmail.com>
2023-06-23 11:28:19 -0700

    Initial Commit

---
+++ interactiveAppTest/interactiveAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveAppTestApp.swift
+//  interactiveAppTest
+//
+//  Created by Marianne Mai on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct interactiveAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
